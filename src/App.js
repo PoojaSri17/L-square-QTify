f87093bc9457b31681b9273c7570ec1a15f5d1e0
Poojasri <poojmahi17@gmail.com>
2024-08-21 21:06:56 +0530

    Add Hero component and integrate it with App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar'; // Import the Navbar component
+import Hero from './components/Hero/Hero'; // Correctly import the Hero component
 import './App.css'; // Import your CSS or global styles
 
 function App() {
   return (
     <div className="App">
       <Navbar /> {/* Add the Navbar component */}
-      <header className="App-header">
-        {/* You can keep or update the existing content */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero /> {/* Add the Hero component */}
     </div>
   );
 }
